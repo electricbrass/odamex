5d023e21ba91d8fee6cb0c4e522bb2356752c288
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-10-05 17:06:26 -0500

    Add workflow for windows on arm

--- ci/win-arm-buildgen.ps1
+++ ci/win-arm-buildgen.ps1
@@ -1,10 +1,10 @@
 Set-PSDebug -Trace 1
 
-mkdir "build-x32" | Out-Null
-Set-Location "build-x32"
+mkdir "build-arm" | Out-Null
+Set-Location "build-arm"
 
 & cmake .. -G "Visual Studio 17 2022" `
-    -A Win32
+    -A ARM64
     -DBUILD_OR_FAIL=1 `
     -DBUILD_CLIENT=1 -DBUILD_SERVER=1 `
     -DBUILD_MASTER=1 -DBUILD_LAUNCHER=1
