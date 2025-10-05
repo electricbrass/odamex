2b2e4964a1facc5bac83c518ba3360535d16b8ba
electricbrass <84749759+electricbrass@users.noreply.github.com>
2025-10-05 03:04:36 +0000

    upversion to version 11.5.0

--- common/version.h
+++ common/version.h
@@ -72,10 +72,10 @@
 // Used by configuration files.  upversion.py will update thie field
 // deterministically and unambiguously so newer versions always compare
 // greater.
-#define CONFIGVERSIONSTR "011011"
+#define CONFIGVERSIONSTR "011050"
 
-#define DOTVERSIONSTR "11.1.1"
-#define GAMEVER (MAKEVER(11, 1, 1))
+#define DOTVERSIONSTR "11.5.0"
+#define GAMEVER (MAKEVER(11, 5, 0))
 
 #define COPYRIGHTSTR "Copyright (C) 2006-2025 The Odamex Team"
 
@@ -91,7 +91,7 @@
 // earlier than this version.  Needs to be exactly 16 chars long.
 // 
 // upversion.py will update thie field deterministically and unambiguously.
-#define SAVESIG "ODAMEXSAVE011011"
+#define SAVESIG "ODAMEXSAVE011050"
 
 #define NETDEMOVER 3
 
